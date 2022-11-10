f715d16ec281e19d8aaf85dd08cddca2055b5260
Cydeamon <Cydeamon@gmail.com>
2022-11-11 00:50:25 +0300

    Graphical hello world. Now I can develop the game

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include "raylib.h"
 using namespace std;
 
 int main()
-{
-    cout << "Hello, world!" << endl;
+{    
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Hello, world!");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        
+        DrawText("Hello, world!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
